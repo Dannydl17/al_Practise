6d295d028e23f973c5279e65f59ecaab99943ac0
Dannydl17 <adeniyidan1612@gmail.com>
2024-08-31 20:22:43 +0100

    personalPractise

--- src/questions/chp5/TwoTableOfKNdP.java
+++ src/questions/chp5/TwoTableOfKNdP.java
@@ -2,46 +2,39 @@ package questions.chp5;
 
 public class TwoTableOfKNdP {
     public static void main(String[] args) {
-        double collectNumber = 0.0;
         double numbers = 2.2;
+        int innerCount = 1;
+        int count = 20;
 
         for (int counter = 0; counter < 1; counter++) {
-            System.out.printf("%5s             %5s    %5s       %5s         %5s%n", "Kilogram", "Pound", "|", "Pound", "Kilogram");
+            System.out.printf("%5s             %5s    %4s       %4s         %4s%n", "Kilogram", "Pound", "|", "Pound", "Kilogram");
         }
 
-        for (int counter = 1; counter <= 5; counter++){
-            for (int numberOfColumn = 1; numberOfColumn <= 3; numberOfColumn++) {
-                for (int nums = 0; nums < 1; nums++) {
-                    for (int count = 1; count < 200; count += 2) {
-
-
-//                        while (count > 3 && count < 196) {
-//                            if (count == 195) {
-//                                System.out.println("  ...");
-//                            }
-//                            count += 2;
-//                        }
-//                        double result = count * numbers;
-//                        System.out.printf("%4d                    %4.1f%n", count, result);
+        for (int counter = 1; counter < 5; counter ++){
+//            for (innerCount = 1; innerCount < 200;innerCount+=2) {
+                while (innerCount > 3 && innerCount < 196){
+                    if (innerCount == 195){
+                        System.out.println("  ...");
                     }
+                    innerCount+=2;
                 }
-            }
-        }
+                double result = innerCount * numbers;
+                System.out.printf("%4d                 %4.1f", innerCount,result);
+                System.out.print("        |");
+                while (count > 25 && count < 510){
+                    if (count == 500) {
+//                        continue;
+                    }
+                    count+=5;
+                }
+                double result1 = count / numbers;
+                System.out.printf("       %4d            %4.2f%n", count,result1);
+                count+=5;
+                innerCount+=2;
 
+        }
     }
 
-    private static int checkNumber(int numbers) {
-        int count = 0;
-         if (numbers > 3 && numbers < 196){
-            if (numbers == 195){
-                return Integer.parseInt("  ...");
-            }
-        }
-         else {
-             count = numbers;
-         }
-        return count;
-    }
 
     private static double performCalculateNumber(int counter, double numbers) {
         double result = 0.0;
