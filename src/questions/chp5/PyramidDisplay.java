e36407b43c124c40f3d2e302fc82104df4795570
Dannydl17 <adeniyidan1612@gmail.com>
2024-09-05 01:03:18 +0100

    codedonetoday

---
+++ src/questions/chp5/PyramidDisplay.java
@@ -0,0 +1,18 @@
+package questions.chp5;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class PyramidDisplay {
+    public static void main(String[] args) {
+        Scanner keyboardInput = new Scanner(in);
+
+        System.out.print("Enter the number of line:  ");
+        int numberOfLine = keyboardInput.nextInt();
+
+        for (int count = 1; count < numberOfLine; count++) {
+
+        }
+    }
+}
