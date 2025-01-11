c3fadc0d6dfac3f43ecf58043967d3b5f5772ee3
Dannydl17 <adeniyidan1612@gmail.com>
2025-01-11 13:13:53 +0100

    codeWritten

---
+++ src/questions/chp6/NumberOfDays.java
@@ -0,0 +1,19 @@
+package questions.chp6;
+
+public class NumberOfDays {
+    public static void main(String[] args) {
+
+    }
+    public static int numberOfDaysInAYear(int year){
+        int numberOfDays = 0;
+
+        if (year % 4 == 0) {
+
+        }
+        else {
+
+        }
+
+        return numberOfDays;
+    }
+}
