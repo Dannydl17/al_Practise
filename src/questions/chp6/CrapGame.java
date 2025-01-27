2d8cdc161f8c73c4a644d6c814acf19a8020b95f
Dannydl17 <adeniyidan1612@gmail.com>
2025-01-27 17:12:02 +0100

    codeWritten

--- src/questions/chp6/CrapGame.java
+++ src/questions/chp6/CrapGame.java
@@ -7,9 +7,9 @@ public class CrapGame {
     public static void main(String[] args) {
         SecureRandom  randomNumber = new SecureRandom();
         int firstGuess = 1 + randomNumber.nextInt(6);
-        System.out.println(firstGuess);
         int secondGuess = 1 + randomNumber.nextInt(6);
-        System.out.println(secondGuess);
+
+        int number = 0;
 
         int sumNum = firstGuess + secondGuess;
 
@@ -17,13 +17,45 @@ public class CrapGame {
             System.out.println("You lose");
         }
 
-        if (sumNum == 7 || sumNum == 11) {
+        if (sumNum == 7) {
+            System.out.println("You lose");
+        }
+
+
+        if (sumNum == 11) {
             System.out.println("You win");
         }
 
         if (sumNum == 4 || sumNum == 5 || sumNum == 6 ||
             sumNum == 8 || sumNum == 9 || sumNum == 10) {
+            number = sumNum;
             System.out.println("point is " + sumNum);
+        }
+
+        while (sumNum == 4 || sumNum == 5 || sumNum == 6 ||
+               sumNum == 8 || sumNum == 9 || sumNum == 10){
+            firstGuess = 1 + randomNumber.nextInt(6);
+            secondGuess = 1 + randomNumber.nextInt(6);
+
+            sumNum = firstGuess + secondGuess;
+
+            if (sumNum == 2 || sumNum == 3 || sumNum == 12) {
+                System.out.println("You lose");
+            }
+
+            if (sumNum == 7) {
+                System.out.println("You lose");
+            }
+
+            if (sumNum == 11) {
+                System.out.println("You win");
+            }
+
+            if (number == sumNum) {
+                System.out.println("You win");
+            }else {
+                System.out.println("You lose");
+            }
         }
     }
 }
