bdb5780b311cc38bfc32394d2a9bf0ec1838993f
Dannydl17 <adeniyidan1612@gmail.com>
2025-01-28 18:17:19 +0100

    codeWritten

--- src/questions/chp6/CrapGame.java
+++ src/questions/chp6/CrapGame.java
@@ -9,7 +9,8 @@ public class CrapGame {
         int firstGuess = 1 + randomNumber.nextInt(6);
         int secondGuess = 1 + randomNumber.nextInt(6);
 
-        int number = 0;
+        int count = 0;
+        int point = 0;
 
         int sumNum = firstGuess + secondGuess;
 
@@ -28,34 +29,38 @@ public class CrapGame {
 
         if (sumNum == 4 || sumNum == 5 || sumNum == 6 ||
             sumNum == 8 || sumNum == 9 || sumNum == 10) {
-            number = sumNum;
-            System.out.println("point is " + sumNum);
+            point = sumNum;
+            System.out.println("point is " + point);
         }
 
-        while (sumNum == 4 || sumNum == 5 || sumNum == 6 ||
-               sumNum == 8 || sumNum == 9 || sumNum == 10){
-            firstGuess = 1 + randomNumber.nextInt(6);
-            secondGuess = 1 + randomNumber.nextInt(6);
 
-            sumNum = firstGuess + secondGuess;
+        if (point == 4 || point == 5 || point == 6||
+            point == 8 || point == 9 || point == 10) {
+            while (count >= 0){
+                firstGuess = 1 + randomNumber.nextInt(6);
+                secondGuess = 1 + randomNumber.nextInt(6);
 
-            if (sumNum == 2 || sumNum == 3 || sumNum == 12) {
-                System.out.println("You lose");
-            }
+                sumNum = firstGuess + secondGuess;
 
-            if (sumNum == 7) {
-                System.out.println("You lose");
-            }
 
-            if (sumNum == 11) {
-                System.out.println("You win");
-            }
+                if(sumNum == 7 || point == sumNum) {
 
-            if (number == sumNum) {
-                System.out.println("You win");
-            }else {
-                System.out.println("You lose");
+                    if (sumNum == 7) {
+                        System.out.println("You lose");
+                    }
+
+                    if (point == sumNum) {
+                        System.out.println("You win");
+                    }
+                    break;
+                }
+
+                if (sumNum != 7) {
+                    count++;
+                }
+
             }
         }
+
     }
 }
