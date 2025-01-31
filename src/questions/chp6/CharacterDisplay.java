0ffded56133eedb0eeace24e76c39ba61f978b57
Dannydl17 <adeniyidan1612@gmail.com>
2025-01-31 01:54:20 +0100

    codeWritten

--- src/questions/chp6/CharacterDisplay.java
+++
@@ -1,7 +0,0 @@
-package questions.chp6;
-
-public class CharacterDisplay {
-    public static void printChars(char ch1, char ch2, int numberPerLine) {
-
-    }
-}
---
+++ src/questions/chp6/DisplayPCharacter.java
@@ -0,0 +1,28 @@
+package questions.chp6;
+
+public class DisplayPCharacter {
+    public static void main(String[] args) {
+        char ch1 = '1';
+        char ch2 = 'Z';
+        int numPerLine = 10;
+        printChars(ch1, ch2, numPerLine);
+    }
+    public static void printChars(char ch1, char ch2, int numberPerLine){
+       int count = 0;
+
+        for (int counter = ch1; counter <= (int) ch2; counter++) {
+//              while (counter > 57 && counter < 65){
+//                  counter++;
+//              }
+              char ch = (char) counter;
+              System.out.print(ch);
+               count++;
+               if (numberPerLine == count) {
+                   System.out.println();
+                   count = 0;
+               }
+        }
+
+
+    }
+}
