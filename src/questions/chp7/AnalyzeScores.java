cbc31027ad80aaf9fa27ef1000aeba05dada5657
Dannydl17 <adeniyidan1612@gmail.com>
2025-03-10 19:40:40 +0100

    codeWritten

--- src/questions/chp7/AnalyzeScores.java
+++ src/questions/chp7/AnalyzeScores.java
@@ -1,22 +1,47 @@
 package questions.chp7;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.System.in;
 
 public class AnalyzeScores {
     public static void main(String[] args) {
+        int averageNum = 50;
+        int numberOfScoreAboveTheAverage = 0;
+        int numberOfScoreBelowTheAverage = 0;
+        ArrayList<Integer> collectScore = new ArrayList<>();
         Scanner keyboardInput = new Scanner(in);
 
 
-        System.out.print("Enter score or enter -1 to break");
+        System.out.print("Enter score or enter -1 to break: ");
         int numberOfScore = keyboardInput.nextInt();
+        collectScore.add(numberOfScore);
 
 
         while (numberOfScore != -1){
-            System.out.print("Enter score or enter -1 to break");
+            System.out.print("Enter score or enter -1 to break: ");
             numberOfScore = keyboardInput.nextInt();
+
+            if (numberOfScore == -1) {
+                int[]num = convertToArray(collectScore);
+                numberOfScoreAboveTheAverage = canKnowTheNumberOfScoresAboveTheAverage(averageNum, num);
+                numberOfScoreBelowTheAverage = canKnowTheNumberOfScoresBelowTheAverage(averageNum, num);
+            }
+            else {
+                collectScore.add(numberOfScore);
+            }
         }
+        System.out.print(numberOfScoreAboveTheAverage);
+        System.out.print(numberOfScoreBelowTheAverage);
+    }
+
+    public static int[] convertToArray(ArrayList<Integer> collectScore) {
+        int[] result = new int[collectScore.size()];
+        for (int count = 0; count < collectScore.size(); count++) {
+             result[count] = collectScore.get(count);
+        }
+        return result;
     }
 
     public static int canKnowTheNumberOfScoresAboveTheAverage(int averageNumber, int... elements) {
