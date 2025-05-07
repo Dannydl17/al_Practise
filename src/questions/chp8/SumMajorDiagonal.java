4bb693e7e41a8fa2a451bd1cc9a77cf96bfb5573
Dannydl17 <adeniyidan1612@gmail.com>
2025-05-07 11:49:10 +0100

    codeWritten

--- src/questions/chp8/SumMajorDiagonal.java
+++ src/questions/chp8/SumMajorDiagonal.java
@@ -1,6 +1,29 @@
 package questions.chp8;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class SumMajorDiagonal {
+
+    public static void main(String[] args) {
+        Scanner keyboardInput = new Scanner(in);
+
+        double[][] nums = new double[4][4];
+
+        System.out.println("Enter a 4-by-4 matrix row by row: ");
+
+        for (int row = 0; row < nums.length; row++) {
+            for (int column = 0; column < nums[row].length; column++) {
+                double number = keyboardInput.nextDouble();
+                nums[row][column] = number;
+            }
+        }
+
+        double answer = canReturnTheSumOfMajorDiagonal(nums);
+
+        System.out.println("Sum of the elements in the major diagonal is " + answer);
+    }
     public static double canReturnTheSumOfMajorDiagonal(double[][] m) {
         int innerColumn = m[0].length;
         double[] collectNumber = new double[innerColumn];
