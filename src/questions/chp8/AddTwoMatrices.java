cedade5026b929da8d3741f97faccd77a0e2feda
Dannydl17 <adeniyidan1612@gmail.com>
2025-05-16 09:52:04 +0100

    codeWritten

--- src/questions/chp8/AddTwoMatrices.java
+++ src/questions/chp8/AddTwoMatrices.java
@@ -1,8 +1,34 @@
 package questions.chp8;
 
 import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.System.in;
 
 public class AddTwoMatrices {
+    public static void main(String[] args) {
+        Scanner keyboardInput = new Scanner(in);
+        double[][] fNumbs = new double[3][3];
+        double[][] sNumbs = new double[3][3];
+
+        System.out.print("Enter matrix1: ");
+        for (int row = 0; row < fNumbs.length; row++) {
+            for (int column = 0; column < fNumbs[row].length; column++) {
+                double numOne = keyboardInput.nextDouble();
+                fNumbs[row][column] = numOne;
+            }
+        }
+
+        System.out.print("Enter matrix2: ");
+        for (int row = 0; row < sNumbs.length; row++) {
+            for (int column = 0; column < sNumbs[row].length; column++) {
+                double numTwo = keyboardInput.nextDouble();
+                sNumbs[row][column] = numTwo;
+            }
+        }
+        System.out.println("The matrices are added as follows");
+
+    }
     public static double[][] addMatrix(double[][] a, double[][] b) {
         double[][] numbers = new double[a.length][b.length];
 
